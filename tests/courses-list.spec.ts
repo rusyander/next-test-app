718cf2a20f840746082ea4fe57169dbfd31392fc
rusyander <rustam.urmanov.1996@mail.ru>
2024-07-24 00:06:28 +0500

    update

--- tests/courses-list.spec.ts
+++ tests/courses-list.spec.ts
@@ -1,16 +1,26 @@
 import { test, expect } from "@playwright/test";
 
-test("create delete courses list", async ({ page }) => {
+test.beforeEach(async ({ page }) => {
   await page.goto("http://localhost:3000/");
-  await page.getByPlaceholder("Имя").click();
-  await page.getByPlaceholder("Имя").fill("test");
-  await page.getByPlaceholder("Описание").click();
-  await page.getByPlaceholder("Описание").fill("test desc");
-  await page.getByRole("button", { name: "Добавить" }).click();
-  await expect(page.getByText("testtest desc")).toBeVisible();
+});
 
-  await page.getByRole("button", { name: "Удалить" }).click();
-  await page.getByRole("button", { name: "Добавить" }).click();
-  await page.getByRole("button", { name: "Удалить" }).click();
-  await expect(page.getByText("testtest desc")).not.toBeVisible();
+test.describe("courses-list", () => {
+  test("create delete courses list", async ({ page }) => {
+    await page.locator("body").click();
+
+    await page.getByPlaceholder("Имя").click();
+    await page.getByPlaceholder("Имя").fill("Test course");
+    await page.getByPlaceholder("Описание").click();
+    await page.getByPlaceholder("Описание").fill("Test description");
+    await page.getByRole("button", { name: "Добавить" }).click();
+    await expect(
+      page.getByText("Test courseTest descriptionУдалить"),
+    ).toBeVisible();
+
+    await page.getByRole("button", { name: "Удалить" }).click();
+
+    await expect(
+      page.getByText("Test courseTest descriptionУдалить"),
+    ).not.toBeVisible();
+  });
 });
