1d645bb2c2b3724b01b9d489799fa728b60db051
rusyander <rustam.urmanov.1996@mail.ru>
2024-07-24 22:39:17 +0500

    fix: auth with gitHub

--- src/features/auth/_vm/use-oauth-sign-in.ts
+++ src/features/auth/_vm/use-oauth-sign-in.ts
@@ -1,9 +1,16 @@
 import { useMutation } from "@tanstack/react-query";
 import { ClientSafeProvider, signIn } from "next-auth/react";
+import { useSearchParams } from "next/navigation";
 
 export function useOAuthSignIn(provider: ClientSafeProvider) {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
+
   const oauthSignInMutation = useMutation({
-    mutationFn: () => signIn(provider.id),
+    mutationFn: () =>
+      signIn(provider.id, {
+        callbackUrl: callbackUrl ?? undefined,
+      }),
   });
 
   return {
