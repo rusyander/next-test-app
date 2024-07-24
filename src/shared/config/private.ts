4b049fc032aa9b663508aa9d696b60f3cb27b049
rusyander <rustam.urmanov.1996@mail.ru>
2024-07-24 21:32:53 +0500

    feat: add email auth (for test, no auth service)

--- src/shared/config/private.ts
+++ src/shared/config/private.ts
@@ -3,6 +3,12 @@ import { z } from "zod";
 const privateConfigSchema = z.object({
   GITHUB_ID: z.string().optional(),
   GITHUB_SECRET: z.string().optional(),
+
+  // EMAIL_SERVER_USER: z.string().optional(),
+  // EMAIL_SERVER_PASSWORD: z.string().optional(),
+  // EMAIL_SERVER_HOST: z.string().optional(),
+  // EMAIL_SERVER_PORT: z.number().optional(),
+  // EMAIL_FROM: z.string().optional(),
 });
 
 export const privateConfig = privateConfigSchema.parse(process.env);
