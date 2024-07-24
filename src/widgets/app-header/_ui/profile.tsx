08c73b32ca4c8ae55b082b3d70b19c56e957d469
rusyander <rustam.urmanov.1996@mail.ru>
2024-07-24 21:03:16 +0500

    feat: add auth with github Oauth

--- src/widgets/app-header/_ui/profile.tsx
+++ src/widgets/app-header/_ui/profile.tsx
@@ -1,6 +1,5 @@
 "use client";
 import { Skeleton } from "@/shared/ui/skeleton";
-// import { useSession } from "next-auth/react";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -13,9 +12,23 @@ import {
 import { LogOut, User } from "lucide-react";
 import { Button } from "@/shared/ui/button";
 import Link from "next/link";
-import { Avatar, AvatarFallback } from "@radix-ui/react-avatar";
+import { SignInButton } from "@/features/auth/sign-in-button.client";
+import { useSignOut } from "@/features/auth/use-sign-out";
+import { useSession } from "next-auth/react";
+import { AvatarFallback, Avatar, AvatarImage } from "@/shared/ui/avatar";
 
 export function Profile() {
+  const session = useSession();
+  const { signOut, isPending: isLoadingSignOut } = useSignOut();
+
+  if (session.status === "loading") {
+    return <Skeleton className="w-8 h-8 rounded-full" />;
+  }
+
+  if (session.status === "unauthenticated") {
+    return <SignInButton />;
+  }
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
@@ -24,6 +37,7 @@ export function Profile() {
           className="p-px rounded-full self-center h-8 w-8"
         >
           <Avatar className="w-8 h-8">
+            <AvatarImage src={session.data?.user.image} />
             <AvatarFallback>AC</AvatarFallback>
           </Avatar>
         </Button>
@@ -32,21 +46,23 @@ export function Profile() {
         <DropdownMenuLabel>
           <p>Мой аккаунт</p>
           <p className="text-xs text-muted-foreground overflow-hidden text-ellipsis">
-            Paromov
+            {session.data?.user.name}
           </p>
         </DropdownMenuLabel>
         <DropdownMenuGroup></DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
           <DropdownMenuItem asChild>
-            {/* <Link href={`/profile/${session.data?.user.id}`}> */}
-            <Link href={`/`}>
+            {/* <Link href={`/profile/${session.data?.user?.id}`}> */}
+            <Link href={`/profile/1`}>
               <User className="mr-2 h-4 w-4" />
               <span>Профиль</span>
             </Link>
           </DropdownMenuItem>
-          {/* <DropdownMenuItem onClick={() => signOut.signOut()}> */}
-          <DropdownMenuItem>
+          <DropdownMenuItem
+            disabled={isLoadingSignOut}
+            onClick={() => signOut()}
+          >
             <LogOut className="mr-2 h-4 w-4" />
             <span>Выход</span>
           </DropdownMenuItem>
